4b4ae9653e4381ee2466cd95ec5e228b9657d319
jml556 <jmleesjc2000@gmail.com>
2021-11-16 10:06:10 -0500

    Update oopRPS.js

--- JS120/lesson1/oopRPS.js
+++ JS120/lesson1/oopRPS.js
@@ -1,13 +1,46 @@
+/*
 const readline = require('readline-sync')
 let play = false;
 
 const question = readline.question('Would you like to play?').toLowerCase()
 
 question == 'yes' ? play = true : play = false;
+*/
+const readline = require('readline-sync')
+rockPaper = {
+  rock: 'rock',
+  paper : 'paper',
+  scissor: 'scissor',
+  userhand: undefined,
+  arrOfHands: ['rock', this.rock],
 
-while(question) {
+  getRandomHand() {
+    console.log(this.arrOfHands)
+    const randomNum = Math.round(Math.random() * 2)
+    return this.arrOfHands[randomNum]
+  },
+
+  displayMessage() {
+    let question = readline.question('Which hand? \nrock, paper, or scissor?')
+    console.log(this.arrOfHands)
+    while(!this.arrOfHands.includes(question)) {
+      question = 'you need to type a proper hand'
+    }
+  }
+}
+
+//rockPaper.getRandomHand()
+
+
+
+/*
+while(play) {
+  console.log('hi')
+  break
   //ask player which hand
   //choose computer hand
   //play again?
     //if not set question to false;
 }
+*/
+
