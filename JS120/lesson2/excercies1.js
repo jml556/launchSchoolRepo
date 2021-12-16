4bbfc50603816b9cb8edad21ac6d3c7b547e68e5
jml556 <jmleesjc2000@gmail.com>
2021-12-16 13:38:19 -0500

    Update excercies1.js

--- JS120/lesson2/excercies1.js
+++ JS120/lesson2/excercies1.js
@@ -756,6 +756,12 @@ Object.assign(Truck.prototype, towMixin)
 let truck = new Truck();
 truck.tow();
 */
+/*
+class Vehicle {
+  constructor(year) {
+    this.year = year;
+  }
+}
 
 const towMixin = {
   tow() {
@@ -763,13 +769,14 @@ const towMixin = {
   }
 }
 
-class Truck {
-  constructor() {
+class Truck extends Vehicle{
+  constructor(year) {
+    super(year)
     Object.assign(this, towMixin);
   }
 }
 
-class Car {}
+class Car extends Vehicle{}
 
 let truck = new Truck(2002);
 console.log(truck.year);
@@ -777,3 +784,238 @@ console.log(truck.tow());
 
 let car = new Car(2015);
 console.log(car.year);
+*/
+
+// name property added to make objects easier to identify
+/*
+let foo = {name: 'foo'};
+let bar = Object.create(foo);
+bar.name = 'bar';
+let baz = Object.create(bar);
+baz.name = 'baz';
+let qux = Object.create(baz);
+qux.name = 'qux';
+
+Object.prototype.ancestors = function() {
+  const arr = [];
+  arr.push(Object.getPrototypeOf(this))
+  while(true) {
+    if(Object.getPrototypeOf(arr[arr.length - 1]) === null) break;
+    arr.push(Object.getPrototypeOf(arr[arr.length - 1]))
+  }
+    console.log(arr)
+}
+
+
+qux.ancestors();  // returns ['baz', 'bar', 'foo', 'Object.prototype']
+baz.ancestors();  // returns ['bar', 'foo', 'Object.prototype']
+bar.ancestors();  // returns ['foo', 'Object.prototype']
+foo.ancestors();  // returns ['Object.prototype']
+*/
+
+/*
+function Person(firstName, lastName, age, gender) {
+  this.firstName = firstName;
+  this.lastName = lastName;
+  this.age = age;
+  this.gender = gender;
+
+  this.fullName = function() {
+    console.log(`${this.firstName + this.lastName}`)
+  }
+
+  this.communicate = function() {
+    console.log('communicate')
+  }
+
+  this.eat = function() {
+    console.log('eat')
+  }
+
+  this.sleep = function() {
+    console.log('sleep')
+  }
+}
+
+
+
+let person = new Person('foo', 'bar', 21, 'gender');
+console.log(person instanceof Person);     // logs true
+person.eat();                              // logs 'Eating'
+person.communicate();                      // logs 'Communicating'
+person.sleep();                            // logs 'Sleeping'
+console.log(person.fullName());            // logs 'foo bar'
+
+let doctor = new Doctor('foo', 'bar', 21, 'gender', 'Pediatrics');
+console.log(doctor instanceof Person);     // logs true
+console.log(doctor instanceof Doctor);     // logs true
+doctor.eat();                              // logs 'Eating'
+doctor.communicate();                      // logs 'Communicating'
+doctor.sleep();                            // logs 'Sleeping'
+console.log(doctor.fullName());            // logs 'foo bar'
+doctor.diagnose();                         // logs 'Diagnosing'
+
+let graduateStudent = new GraduateStudent('foo', 'bar', 21, 'gender', 'BS Industrial Engineering', 'MS Industrial Engineering');
+// logs true for next three statements
+console.log(graduateStudent instanceof Person);
+console.log(graduateStudent instanceof Student);
+console.log(graduateStudent instanceof GraduateStudent);
+graduateStudent.eat();                     // logs 'Eating'
+graduateStudent.communicate();             // logs 'Communicating'
+graduateStudent.sleep();                   // logs 'Sleeping'
+console.log(graduateStudent.fullName());   // logs 'foo bar'
+graduateStudent.study();                   // logs 'Studying'
+graduateStudent.research();                // logs 'Researching'
+*/
+
+let readline = require("readline-sync");
+
+class Square {
+  static UNUSED_SQUARE = " ";
+  static HUMAN_MARKER = "X";
+  static COMPUTER_MARKER = "O";
+
+  constructor(marker = Square.UNUSED_SQUARE) {
+    this.marker = marker;
+  }
+
+  toString() {
+    return this.marker;
+  }
+
+  setMarker(marker) {
+    this.marker = marker;
+  }
+}
+
+class Board {
+  constructor() {
+    this.squares = {};
+    for (let counter = 1; counter <= 9; ++counter) {
+      this.squares[String(counter)] = new Square();
+    }
+  }
+
+  display() {
+    console.log("");
+    console.log("     |     |");
+    console.log(`  ${this.squares["1"]}  |  ${this.squares["2"]}  |  ${this.squares["3"]}`);
+    console.log("     |     |");
+    console.log("-----+-----+-----");
+    console.log("     |     |");
+    console.log(`  ${this.squares["4"]}  |  ${this.squares["5"]}  |  ${this.squares["6"]}`);
+    console.log("     |     |");
+    console.log("-----+-----+-----");
+    console.log("     |     |");
+    console.log(`  ${this.squares["7"]}  |  ${this.squares["8"]}  |  ${this.squares["9"]}`);
+    console.log("     |     |");
+    console.log("");
+  }
+
+  markSquareAt(key, marker) {
+    this.squares[key].setMarker(marker);
+  }
+}
+
+class Row {
+  constructor() {
+    // We need some way to identify a row of 3 squares
+  }
+}
+
+class Player {
+  constructor(marker) {
+    this.marker = marker;
+  }
+
+  getMarker() {
+    return this.marker;
+  }
+
+  play() {
+    // We need a way for each player to play the game.
+    // Do we need access to the board?
+  }
+}
+
+class Human extends Player {
+  constructor() {
+    super(Square.HUMAN_MARKER);
+  }
+}
+
+class Computer extends Player {
+  constructor() {
+    super(Square.COMPUTER_MARKER);
+  }
+}
+
+class TTTGame {
+  constructor() {
+    this.board = new Board();
+    this.human = new Human();
+    this.computer = new Computer();
+  }
+
+  play() {
+    this.displayWelcomeMessage();
+
+    while (true) {
+      this.board.display();
+
+      this.humanMoves();
+      this.board.display(); // so we can see human's move
+      if (this.gameOver()) break;
+
+      this.computerMoves();
+      this.board.display(); // so we can see the computer's move
+      if (this.gameOver()) break;
+      break; // <= execute loop only once for now
+    }
+
+    this.displayResults();
+    this.displayGoodbyeMessage();
+  }
+
+  displayWelcomeMessage() {
+    console.log("Welcome to Tic Tac Toe!");
+  }
+
+  displayGoodbyeMessage() {
+    console.log("Thanks for playing Tic Tac Toe! Goodbye!");
+  }
+
+  displayResults() {
+    // show the results of this game (win, lose, tie)
+  }
+
+  humanMoves() {
+    let choice;
+
+    while (true) {
+      choice = readline.question("Choose a square between 1 and 9: ");
+
+      let integerValue = parseInt(choice, 10);
+      if (integerValue >= 1 && integerValue <= 9) {
+        break;
+      }
+
+      console.log("Sorry, that's not a valid choice.");
+      console.log("");
+    }
+
+    this.board.markSquareAt(choice, this.human.getMarker());
+  }
+
+  computerMoves() {
+    let choice = Math.floor((9 * Math.random()) + 1);
+    this.board.markSquareAt(choice, this.computer.getMarker());
+  }
+
+  gameOver() {
+    return false;
+  }
+}
+
+let game = new TTTGame();
+game.play();
