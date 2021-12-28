43cdf68808ead5c43b9323a193effe78eb381934
jml556 <jmleesjc2000@gmail.com>
2021-12-27 19:26:27 -0500

    new todo list

--- JS130/forEach.js
+++ JS130/forEach.js
@@ -1,3 +1,5 @@
+
+/*
 //forEach(arr, value => console.log(value * value));
 function forEach(arr, func, thisArg = null) {
   const newArr = arr.slice()
@@ -30,3 +32,102 @@ forEach(arr, foo.showItem, foo);
 forEach([4, 5, 6], foo.showItem, foo);
 */
 
+/* filter
+
+function filter(arr, callback) {
+  let counter = 0;
+  const returnArr = []
+  for(let i= 0; i < arr.length; i++) {
+    if(callback(arr[i])) {
+      returnArr[counter] = arr[i]
+      counter++
+    }
+  }
+  return returnArr
+}
+
+
+let numbers = [1, 2, 3, 4, 5];
+console.log(filter(numbers, number => number > 3)); // => [ 4, 5 ]
+console.log(filter(numbers, number => number < 0)); // => []
+console.log(filter(numbers, () => true));           // => [ 1, 2, 3, 4, 5 ]
+
+let values = [1, "abc", null, true, undefined, "xyz"];
+console.log(filter(values, value => typeof value === "string"));
+// => [ 'abc', 'xyz' ]
+*/
+
+/*
+let numbers = [1, 2, 3, 4, 5];
+console.log(map(numbers, number => number * 3));  // => [ 3, 6, 9, 12, 15 ]
+console.log(map(numbers, number => number + 1));  // => [ 2, 3, 4, 5, 6 ]
+console.log(map(numbers, () => false));
+// => [ false, false, false, false, false ]
+
+let values = [1, "abc", null, true, undefined, "xyz"];
+console.log(map(values, value => String(value)));
+// => [ '1', 'abc', 'null', 'true', 'undefined', 'xyz' ]
+
+
+function map(arr, callback) {
+  const mappedValues = []
+  const copyArr = arr.slice()
+  for(let i = 0; i < copyArr.length; i++) {
+    mappedValues.push(callback(copyArr[i]))
+  }
+  return mappedValues;
+}
+*/
+/*
+const peopleArr  = [
+  {
+      username:    'glestrade',
+      displayname: 'Inspector Lestrade',
+      email:       'glestrade@met.police.uk',
+      authHash:    'bdbf9920f42242defd9a7f76451f4f1d',
+      lastSeen:    '2019-05-13T11:07:22+00:00',
+  },
+  {
+      username:    'mholmes',
+      displayname: 'Mycroft Holmes',
+      email:       'mholmes@gov.uk',
+      authHash:    'b4d04ad5c4c6483cfea030ff4e7c70bc',
+      lastSeen:    '2019-05-10T11:21:36+00:00',
+  },
+  {
+      username:    'iadler',
+      displayname: 'Irene Adler',
+      email:       null,
+      authHash:    '319d55944f13760af0a07bf24bd1de28',
+      lastSeen:    '2019-05-17T11:12:12+00:00',
+  },
+];
+let counter = 0;
+
+const peopleObj = peopleArr.reduce((acc, item) => {
+
+  return acc['lmao'] = item
+}, {})
+
+console.log(peopleObj)
+*/
+/*
+
+const arrOfStrings = ['hello', 'hi', 'world', 'hh'].reduce((acc, ele) => {
+  if(ele[0] == 'h') {
+    return acc.concat(ele)
+  }
+  else {
+    return acc;
+  }
+},[])
+
+console.log(arrOfStrings)
+*/
+
+const arr = [[1], [2], [3], [4]].reduce((acc, ele) => {
+  return [acc.concat(...ele).reduce((acc, ele) => acc + ele)]
+}, [])
+
+
+console.log(arr)
--- JS130/todoList.js
+++ JS130/todoList.js
@@ -1,3 +1,6 @@
+
+/*
+First iteration
 class List {
   constructor(type) {
     this.type = type;
@@ -32,4 +35,104 @@ console.log(laundryList)
 remove('shirt')
 
 console.log(laundryList)
+*/
+
+
+
+class TodoList {
+  constructor(type, date) {
+    this.type = type;
+    this.time = null;
+    if(this.time == null) {
+      this.time = date;
+    }
+  }
+
+  static todoItems = [];
+
+  addItem(item) {
+    TodoList.todoItems.push(item)
+  }
+
+  removeItem(item) {
+    TodoList.todoItems = TodoList.todoItems.filter(ele => {
+      ele.title !== item.title
+    })
+  }
+
+  forEach(array, func) {
+    const copyArr = array.slice()
+    for(let i = 0; i < array.length; i++) {
+      func(copyArr[i])
+    }
+  }
+}
+
+class ToDoItem extends TodoList {
+  constructor(title, description, date, type) {
+    super(type, date)
+    this.title = title;
+    this.description = description;
+    this.date = date;
+    this.complete = false;
+    this.addItem({title, description, date})
+  }
+
+  done() {
+    this.complete = true;
+  }
+
+  revert() {
+    this.complete = !this.complete
+  }
+}
+
+const item1 = new ToDoItem('pickup', 'go to the warehouse', '12/21/2021', 'active')
+const item2 = new ToDoItem('pickup1', 'go to the warehouse', '12/22/2021', 'active1')
+const item3 = new ToDoItem('pickup2', 'go to the warehouse', '12/23/2021', 'active2')
+const item4 = new ToDoItem('pickup3', 'go to the warehouse', '12/24/2021', 'active3')
+
+TodoList.prototype.forEach(TodoList.todoItems, (item) => {
+  console.log(item)
+})
+
+
+
+
+
+// omitted code
+
+// This class represents a collection of Todo objects.
+// You can perform typical collection-oriented actions
+// on a TodoList object, including iteration and selection.
+/*
+
+class TodoList {
+  constructor(title) {
+    this.title = title;
+    this.todos = [];
+  }
+
+  // rest of class needs implementation
+}
+
+let list = new TodoList("Today's Todos");
+console.log(list); // TodoList { title: "Today's Todos", todos: [] }
+/*
+// Omitted code
+
+let todo1 = new Todo("Buy milk");
+let todo2 = new Todo("Clean room");
+let todo3 = new Todo("Go to the gym");
+let todo4 = new Todo("Go shopping");
+
+list.add(todo1);
+list.add(todo2);
+list.add(todo3);
+list.add(todo4);
+console.log(list);
+
+list.add(1); // delete this line after testing it
+*/
+
 
