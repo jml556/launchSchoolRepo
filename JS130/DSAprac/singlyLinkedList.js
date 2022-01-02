db4c7b9dcd918676dd9b2e13097cd7580920e1a1
jml556 <jmleesjc2000@gmail.com>
2022-01-01 19:17:12 -0500

    Create singlyLinkedList.js

---
+++ JS130/DSAprac/singlyLinkedList.js
@@ -0,0 +1,36 @@
+
+class Node {
+  constructor(val) {
+    this.val = val;
+    this.next = null;
+  }
+}
+
+class SinglyLinkedList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.length = 0;
+  }
+
+  push(item) {
+    const newNode = new Node(item)
+    if(!this.head) {
+      this.head = newNode;
+      this.tail = newNode;
+    }
+    else {
+      this.tail.next = newNode;
+      this.tail = newNode;
+    }
+    this.length++
+    return this
+  }
+}
+/*
+{
+  val = 'ads',
+  next = null;
+}
+
+*/
